a9074da82b6632805f5b46f70f51e602fde7eb85
Prizrak9 <pavelgolotuk@gmail.com>
2024-04-25 09:51:08 +0300

    Add project files.

---
+++ AzgaarToCK3/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AzgaarToCK3;
+
+
+public record PackProvince(int i, int state, int burg, string name);
+
+// For some reason the first element in the array isn't an object but a number.
+// Need custom converter to skip the number and parse all other provinces.
+public class PackProvinceJsonConverter : JsonConverter<PackProvince[]>
+{
+    public override PackProvince[]? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        using var jsonDocument = JsonDocument.ParseValue(ref reader);
+        var str = jsonDocument.RootElement.GetRawText();
+
+        // replace 0 with empty province (sea).
+        var escapedStr = string.Concat(str.AsSpan(0, 1),"{}", str.AsSpan(2));
+
+        var provinces = JsonSerializer.Deserialize<PackProvince[]>(escapedStr);
+
+        return provinces;
+    }
+
+    public override void Write(Utf8JsonWriter writer, PackProvince[] value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Pack
+{
+    [JsonConverter(typeof(PackProvinceJsonConverter))]
+    public PackProvince[] provinces { get; set; }
+}
+
+
+public record JsonMap(Pack pack);
+
+
+public record Geometry(string type, float[][][] coordinates);
+public record Properties(string type, int province, int state, int height);
+public record Feature(Geometry geometry, Properties properties);
+public record GeoMap(Feature[] features);
+
+
+//public record Province(List<float[][]> cells, MagickColor color);
+public record Cell(int height, float[][] cells);
+public class Province 
+{
+    public List<Cell> Cells { get; set; } = new();
+    public MagickColor Color { get; set; }
+    public string Name { get; set; }
+}
+
+
+public class Map
+{
+    //public PointD[][] Coordinates { get; set; }
+    public GeoMap GeoMap { get; set; }
+    public JsonMap JsonMap { get; set; }
+    public float XOffset { get; set; }
+    public float YOffset { get; set; }
+    public float XRatio { get; set; }
+    public float YRatio { get; set; }
+
+    public Province[] Provinces { get; set; }
+    //public Dictionary<int, int[]> Provinces { get; set; }
+}
+
+
+public class MyJsonTypeInfoResolver : IJsonTypeInfoResolver
+{
+    public JsonTypeInfo? GetTypeInfo(Type type, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public static async Task Test()
+    {
+        var geoMap = await MapManager.LoadGeojson();
+        var jsonMap = await MapManager.LoadJson();
+        var map = await MapManager.ConvertMap(geoMap, jsonMap);
+
+        //await MapManager.DrawCells(map);
+        await MapManager.DrawProvinces(map);
+        //await MapManager.DrawHeightMap(map);
+        //await MapManager.WriteDefinition(map);
+
+        Application.Current.Shutdown();
+    }
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        _ = Test();
+
+        //Task.Run(() => MapManager.LoadTest());
+       
+        //var map = MapManager.Load("Mones Full 2024-04-20-12-52.json");
+
+    }
+}
---
+++ AzgaarToCK3/MapManager.cs
@@ -0,0 +1,273 @@
+﻿using ImageMagick;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AzgaarToCK3;
+
+public static class MapManager
+{
+    private const int MapWidth = 8192;
+    private const int MapHeight = 4096;
+    private const int WaterLevelHeight = 30;
+
+
+    public static async Task<GeoMap> LoadGeojson()
+    {
+        try
+        {
+            var file = await File.ReadAllTextAsync("input.geojson");
+            var geomap = JsonSerializer.Deserialize<GeoMap>(file);
+            return geomap;
+        }
+        catch(Exception e)
+        {
+            Debugger.Break();
+            return null;
+        }
+      
+    }
+    public static async Task<JsonMap> LoadJson()
+    {
+        try
+        {
+            var file = await File.ReadAllTextAsync("input.json");
+
+            var jsonmap = JsonSerializer.Deserialize<JsonMap>(file);
+            return jsonmap;
+        }
+        catch (Exception e)
+        {
+            Debugger.Break();
+            return null;
+        }
+
+    }
+
+    private static MagickColor GetColor(int i, int maxI)
+    {
+        // max 24bit color
+        var maxColor = 256 * 256 * 256;
+
+        var color = maxColor / maxI * i;
+
+        byte r = (byte)((color & 0x0000FF) >> 0);
+        byte g = (byte)((color & 0x00FF00) >> 8);
+        byte b = (byte)((color & 0xFF0000) >> 16);
+
+        return new MagickColor(r, g, b);
+    }
+
+    private static Dictionary<int, Province> GetProvinceCells(GeoMap geomap)
+    {
+        var provinces = new Dictionary<int, Province>();
+        foreach (var feature in geomap.features)
+        {
+            var provinceId = feature.properties.province;
+
+            if (!provinces.ContainsKey(provinceId))
+            {
+                provinces[provinceId] = new Province();
+            }
+
+            var cells = feature.geometry.coordinates.Select(n => new Cell(feature.properties.height, n));
+            provinces[provinceId].Cells.AddRange(cells);
+        }
+        return provinces;
+    }
+    
+    private static Province[] CreateProvinces(GeoMap geomap, JsonMap jsonmap)
+    {
+        var provinceCells = GetProvinceCells(geomap);
+        var provinces = new Province[provinceCells.Count];
+
+        // pId == 0 is ocean.
+        provinces[0] = provinceCells[0];
+        provinces[0].Color = MagickColors.Black;
+        provinces[0].Name = "x";
+
+        int i = 1;
+        try
+        {
+            for (; i < provinces.Length; i++)
+            {
+                var color = GetColor(i, provinces.Length);
+                provinces[i] = provinceCells[i];
+                provinces[i].Color = color;
+                provinces[i].Name = jsonmap.pack.provinces[i].name;
+            }
+        }
+        catch(Exception ex)
+        {
+            Debugger.Break();
+        }
+       
+
+        return provinces;
+    }
+
+    public static async Task<Map> ConvertMap(GeoMap geoMap, JsonMap jsonMap)
+    {
+        var flatCoordinates = geoMap!.features.SelectMany(n => n.geometry.coordinates).SelectMany(n => n);
+
+        // Cells
+        var maxX = flatCoordinates.MaxBy(n => n[0])![0];
+        var maxY = flatCoordinates.MaxBy(n => n[1])![1];
+
+        var minX = flatCoordinates.MinBy(n => n[0])![0];
+        var minY = flatCoordinates.MinBy(n => n[1])![1];
+
+        var xRatio = MapWidth / (Math.Abs(minX) + Math.Abs(maxX));
+        var yRatio = MapHeight / (Math.Abs(minY) + Math.Abs(maxY));
+
+        var map = new Map
+        {
+            GeoMap = geoMap,
+            XOffset= minX,
+            YOffset= minY,
+            XRatio = xRatio,
+            YRatio = yRatio,
+            Provinces = CreateProvinces(geoMap, jsonMap),
+        };
+
+        return map;
+    }
+
+    public static async Task DrawCells(Map map)
+    {
+        try
+        {
+            var settings = new MagickReadSettings()
+            {
+                Width = MapWidth,
+                Height = MapHeight,
+            };
+            using var cellsMap = new MagickImage("xc:white", settings);
+
+            var drawables = new Drawables();
+            foreach (var feature in map.GeoMap.features)
+            {
+                foreach (var cell in feature.geometry.coordinates)
+                {
+                    drawables
+                        .Polygon(cell.Select(n => new PointD((n[0] - map.XOffset) * map.XRatio, MapHeight - (n[1] - map.YOffset) * map.YRatio)))
+                        .DisableStrokeAntialias()
+                        .StrokeWidth(2)
+                        .StrokeColor(MagickColors.Black)
+                        .FillOpacity(new Percentage(0));
+                }
+            }
+
+            cellsMap.Draw(drawables);
+            await cellsMap.WriteAsync($"{Environment.CurrentDirectory}/cells.png");
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+      
+
+    }
+    public static async Task DrawProvinces(Map map)
+    {
+        try
+        {
+            var settings = new MagickReadSettings()
+            {
+                Width = MapWidth,
+                Height = MapHeight,
+            };
+            using var cellsMap = new MagickImage("xc:#5C5D0B", settings);
+
+            var drawables = new Drawables();
+            foreach (var province in map.Provinces.Skip(1))
+            {
+                foreach (var cell in province.Cells)
+                {
+                    drawables
+                        .Polygon(cell.cells.Select(n => new PointD((n[0] - map.XOffset) * map.XRatio, MapHeight - (n[1] - map.YOffset) * map.YRatio)))
+                        .DisableStrokeAntialias()
+                        .StrokeColor(province.Color)
+                        .FillColor(province.Color);
+                }
+            }
+
+            cellsMap.Draw(drawables);
+            await cellsMap.WriteAsync($"{Environment.CurrentDirectory}/provinces.png");
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+
+
+    }
+    public static async Task DrawHeightMap(Map map)
+    {
+        try
+        {
+            var maxHeight = map.Provinces.Skip(1).SelectMany(n => n.Cells).MaxBy(n => n.height)!.height;
+            var minHeight = map.Provinces.Skip(1).SelectMany(n => n.Cells).MinBy(n => n.height)!.height;
+
+            var settings = new MagickReadSettings()
+            {
+                Width = MapWidth,
+                Height = MapHeight,
+            };
+            using var cellsMap = new MagickImage("xc:black", settings);
+
+            var drawables = new Drawables();
+            foreach (var province in map.Provinces.Skip(1))
+            {
+                foreach (var cell in province.Cells)
+                {
+                    var trimmedHeight = cell.height * (255 - WaterLevelHeight) / maxHeight + WaterLevelHeight;
+                    if (trimmedHeight is > 255 or < WaterLevelHeight)
+                    {
+                        int i = 0;
+                    }
+
+                    var culledHeight = (byte)trimmedHeight;
+
+                    if (culledHeight < WaterLevelHeight)
+                    {
+                        int i = 0;
+                    }
+
+                    var color = new MagickColor(culledHeight, culledHeight, culledHeight);
+                    drawables
+                        .Polygon(cell.cells.Select(n => new PointD((n[0] - map.XOffset) * map.XRatio, MapHeight - (n[1] - map.YOffset) * map.YRatio)))
+                        .DisableStrokeAntialias()
+                        .StrokeColor(color)
+                        .FillColor(color);
+                }
+            }
+
+            cellsMap.Draw(drawables);
+            var outName = $"{Environment.CurrentDirectory}/heightmap.png";
+            await cellsMap.WriteAsync(outName);
+
+            using var file = await SixLabors.ImageSharp.Image.LoadAsync(outName);
+            file.Mutate(n => n.GaussianBlur(15));
+            file.Save(outName);
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+    public static async Task WriteDefinition(Map map)
+    {
+        var lines = map.Provinces.Select((n, i) => $"{i};{n.Color.R};{n.Color.G};{n.Color.B};{n.Name};x;");
+        await File.WriteAllLinesAsync("definition.csv", lines);
+    }
+}
