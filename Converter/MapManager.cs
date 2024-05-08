b471fbeabfc70e41049d46775a32656180781bb4
Prizrak9 <pavelgolotuk@gmail.com>
2024-05-08 21:30:10 +0300

    fixed water provinces breaking land provinces

--- Converter/MapManager.cs
+++ Converter/MapManager.cs
@@ -195,7 +195,7 @@ public static class MapManager
             var cells = waterProvince.Cells;
 
             var largestWaterProvince = cells.MaxBy(n => n.area).area;
-            var areaPerProvince = largestWaterProvince / 2;
+            var areaPerProvince = largestWaterProvince * 1.5;
 
             var unprocessedCells = waterProvince.Cells.ToDictionary(n => n.id, n => n);
             var provinces = new List<Province>();
