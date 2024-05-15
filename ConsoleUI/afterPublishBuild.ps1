5a7c0bd62a55e69591633a3ae08d302011336a41
Prizrak9 <pavelgolotuk@gmail.com>
2024-05-15 12:07:10 +0300

    added macos

--- ConsoleUI/afterPublishBuild.ps1
+++ ConsoleUI/afterPublishBuild.ps1
@@ -16,6 +16,8 @@ Remove-Item "$OutDir" -Recurse
 #dotnet publish -r win-arm /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o "$($OutDir)win-arm"
 
 dotnet publish -c Release -r win-x64 /p:PublishAot=true /p:PublishReadyToRun=true /p:IncludeNativeLibrariesForSelfExtract=true /p:DebugType=None /p:DebugSymbols=false /p:CopyOutputSymbolsToPublishDirectory=false --self-contained true -o "$($OutDir)win-x64"
+dotnet publish -c Release -r osx-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:DebugType=None /p:DebugSymbols=false /p:CopyOutputSymbolsToPublishDirectory=false --self-contained true -o "$($OutDir)osx-x64"
 
 # Create Archive
 Compress-Archive "$($OutDir)win-x64\*" "$($SolutionDir)AzgaarToCK3_$($version)_win-x64.zip" -force
+Compress-Archive "$($OutDir)osx-x64\*" "$($SolutionDir)AzgaarToCK3_$($version)_osx-x64.zip" -force
--- changelog.md
+++ changelog.md
@@ -1,5 +1,6 @@
 # 1.2.3
 - Switched to AOT compilation for publishing.
+- Added MacOS (not AOT as cross-OS compilationis not possible).
 
 # 1.2.2
 - Added publishing for multiple platforms.
